71593f859b6bdf33eb5878240ff41de3cc295b87
glahaie <glahaie@gmail.com>
2014-09-08 19:23:58 -0400

    Added solutions for duplicate and filter-cap

--- clojure_tutorials/src/clojure_tutorials/core.clj
+++ clojure_tutorials/src/clojure_tutorials/core.clj
@@ -14,3 +14,23 @@
     (if (empty? x)
       max-val
       (recur (rest x) (if (> max-val (first x)) max-val (first x))))))
+
+;;Filter out all the non-capital letters from a string
+(defn filter-caps
+  "Removes all non capital letters from a string"
+  [x]
+  (clojure.string/replace x #"[^A-Z]" ""))
+
+;; Must reverse the reverse for the right order
+(defn duplicate 
+  "Duplicates the elements of a sequence"
+  [x]
+  (loop [y x new_seq '()]
+    (if (empty? y)
+      new_seq
+      (recur (rest y) (conj new_seq (first y) (first y))))))
+
+;; This seems like the best solution, using interleave, which
+;; creates a sequence by taking the 1st elements of the 2 collections, then
+;; the second, etc..
+;; #(interleave % %)
