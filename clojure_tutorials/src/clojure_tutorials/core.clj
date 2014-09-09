b447120f8670e5bd8da672b8df8662ba20eec731
glahaie <glahaie@gmail.com>
2014-09-08 22:16:48 -0400

    More koans

--- clojure_tutorials/src/clojure_tutorials/core.clj
+++ clojure_tutorials/src/clojure_tutorials/core.clj
@@ -46,3 +46,48 @@
 ;; I like this solution the best, I think we are looking for take-while here
 ;; #(take-while (fn [x] (< x %2)) (iterate inc %1))
 ;; here, the x represents the values in the list created by iterate
+
+(defn factorial
+  "[int] Calculates the factorial of a value."
+  [x]
+  (if (= x 1)
+    1
+    (* (factorial (dec x)) x)))
+
+;; An interesting solution, using range
+;;#(reduce * (range 1 (inc %)))
+
+;;Interleave two sequences without using interleave
+;;This wouldn't work for collections of lists, for example, because of
+;; the use of flatten
+(defn my-interleave
+  "[coll1 coll2] Interleaves the values of both collections.
+   We only include as many elements in the interleave as there are
+   in the shortest collection."
+  [coll1 coll2]
+  (flatten (take (min (count coll1) (count coll2)) (map list coll1 coll2))))
+
+;;Here is a solution without flatten, but it uses loop again
+(defn my-interleave2 
+  "[coll coll] interleaves elements from the two collections"
+  [coll1 coll2]
+  (loop [x coll1 y coll2 result ,()]
+    (if (or (empty? x) (empty? y))
+      (reverse result)
+      (recur (rest x) (rest y) (conj result (first x) (first y))))))
+
+;; Simplest solution here seems to be 
+;;mapcat list, mapcat creating a concat of the application of the function
+;; to the arguments
+
+
+;;Compress a sequence: remove all elements that are the same as the one before
+(defn compress
+  "[coll] Removes elements that are the same, one after the other"
+  [coll]
+  (reduce #(if (= (last %1) %2) %1 (concat %1 (list %2))) '() (seq coll)))
+
+;;I couldn't get a loop version to work. Also, I can't use when in reduce, because
+;;I always have to return a value in the fn given to reduce.
+;;This seems like the most popular solution:
+;;#(map first (partition-by identity %))
